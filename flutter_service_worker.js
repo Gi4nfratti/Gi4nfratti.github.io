9adeba279ce4741ed32c6f38e792a4c2ae2d5131
André Gianfratti <32543785+Gi4nfratti@users.noreply.github.com>
2025-01-22 12:24:55 -0300

    Update contact page

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -97,7 +97,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "04251941132ba5289c4bd173502087b2
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.ico": "00dc748ea31df3074f00af8326bae3bb",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "722406a7c78ebca8eee1b3a796112292",
+"flutter_bootstrap.js": "855f0a2292108abf4632ee15929c1b38",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
